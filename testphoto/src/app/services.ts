af7d0365846102dd033d667f86c2636784846038
tardismug <123480466+tardismug@users.noreply.github.com>
2023-01-26 17:01:24 +0100

    2 files modified

--- testphoto/src/app/services.ts
+++ testphoto/src/app/services.ts
@@ -1,5 +1,11 @@
-export interface Hero {
+export interface Services {
   id: number;
   titre: string;
   image_01: string;
 }
+export const SERVICES: Services[] = [
+  { id: 1, titre: 'Mariage', image_01: 'xxx' },
+  { id: 2, titre: 'Immobilier', image_01: 'xxx' },
+  { id: 3, titre: 'Famille', image_01: 'xxx' },
+  { id: 4, titre: 'Voyages', image_01: 'xxx' },
+];
--- testphoto/src/app/services/services.component.ts
+++ testphoto/src/app/services/services.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
+import { Services } from '../services';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
-export class ServicesComponent {
-
-
-}
+export class ServicesComponent {}
